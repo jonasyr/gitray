ed109e3021822370f90d57ba1b58e9bbce57ce90
jonasyr <jonas.weirauch@gmx.de>
2025-05-20 00:13:53 +0200

    Remove compiled JavaScript files from version control

--- apps/backend/src/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = errorHandler;
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json({ error: err.message });
-}
